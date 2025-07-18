310fd26ebd4e4f2fe615002a0e0da1b77731e26c
Dylan Moreira <moreiradanix@gmail.com>
2025-07-17 23:54:02 -0300

    cajero.py

--- cajero.py
+++ cajero.py
@@ -1,69 +1,81 @@
-passw = 8388
 
+# Este programa simulara el funcionamiento de un cajero automatico
 
-# Solicitud y verificacion del pin ingresado
+# Creamos el usuario en el banco con esta clase
+class UsuarioBanco():
+    def __init__(self,nombre,num_cuenta,pin):
+        self.nombre = nombre
+        self.num_cuenta = num_cuenta
+        self.pin = pin
 
-while True:
-    pas = int(input("Ingrese su pin: "))   
-    if pas == passw:
-        break
+# Este objeto seria el usuario, ya con sus parametros
+Usuario = UsuarioBanco("Martin",49503212,8388)
+saldo_usuario = 0
+
+# Funcion que permite calcular el saldo del usuario
+def ConsultarSaldo():
+    print("-" * 40)
+    print("Consultar saldo")
+    IngresarCuenta = int(input("Ingrese su numero de cuenta: "))
+    IngresePing = int(input("Ingrese su pin: "))
+    if IngresarCuenta == Usuario.num_cuenta and IngresePing == Usuario.pin:
+        print(f"Su saldo actual es de ${saldo_usuario}")
     else:
-        print("ERROR: Pin incorrecto")
+        print("ERROR: Datos incorrectos")
 
+# Funcion que permite cargar saldo
+def CargarSaldo():
+    global saldo_usuario
+    print("-" * 40)
+    print("Cargar saldo a una cuenta")
+    IngresarCuenta = int(input("Ingrese su numero de cuenta: "))
+    if IngresarCuenta == Usuario.num_cuenta:
+        SaldoACargar = int(input("¿Cuanto desea cargar? "))
+        saldo_usuario = saldo_usuario + SaldoACargar
+        print(f"Cargaste correctamente {SaldoACargar} en la cuenta: {Usuario.num_cuenta}")
+    else:
+        print("ERROR: Datos incorrectos")
 
-#  // Sistema de conteo de valores
+# Funcion para retirar dinero de su cuenta 
+def RetirarSaldo():
+    global saldo_usuario
+    print("-" * 40)
+    print("Retirar saldo de una cuenta")
+    IngresarCuenta = int(input("Ingrese su numero de cuenta: "))
+    IngresePing = int(input("Ingrese su pin: "))
+    if IngresarCuenta == Usuario.num_cuenta and IngresePing == Usuario.pin:
+        RetirarSaldo = int(input("¿Cuanto desea retirar? "))
+        if RetirarSaldo > saldo_usuario:
+            print("No tiene saldo suficiente")         
+        elif RetirarSaldo < saldo_usuario:
+            saldo_usuario = saldo_usuario - RetirarSaldo
+            print("Saldo retirado con exito")
+    else:
+        print("Datos incorrectos")
+            
 
-# arch = open("valores.txt","r")
-## añadir sistema de open para poder actualizar cuenta del usuario en el momento y registrar sus datos ##
-     # SOLUCIONAR ERROR
-cuenta_bancaria = 20300
-
-print("// Menu de finanzas //") 
-print("")
-print("1 - Consultar saldo")    
-print("2 - Retirar dinero")    
-print("3 - Cargar dinero")   
-print("4 - Ayuda")   
-print("5- Salir del menu")      
-    
-print("")
-opcion = int(input("Ingrese una opcion: "))
-
-while True:
-    try:
-        if opcion == 1:
-            print(f" ${cuenta_bancaria}")
-            break
-        elif opcion == 2:
-            retirar = int(input("Ingrese el monto a retirar "))
-            if retirar < cuenta_bancaria:
-                print(f"Dinero retirado: $ {retirar}")
-                cuenta_bancaria -= retirar
-                print(f"Saldo actual: $ {cuenta_bancaria}")
+# Esta funcion contiene el menu
+def Menu():
+    while True:
+        try:
+            print("-" * 40)
+            print("Cajero automatico AzuraCash")
+            print("1 - Consultar saldo")
+            print("2 - Cargar dinero a la cuenta")
+            print("3 - Retirar efectivo")
+            print("4 - Terminar")
+            opcion_menu = int(input("¿Que movimiento deseeas hacer? "))
+            if opcion_menu == 4:
+                print("Cerrando sesion")
                 break
-            elif retirar > cuenta_bancaria:
-                print("ERROR: No tienes saldo suficiente")
-                
-        elif opcion == 3:
-            carga = int(input("Ingrese el monto a cargar: "))
-            cuenta_bancaria += carga
-            print(f"Dinero cargado a su cuenta: {carga}")
-            print(f"Saldo total actual {cuenta_bancaria}")
-            break
-        elif opcion == 4:
-            print("")
-            print("Menu de ayuda")
-            print("1 - cambiar pin")  # AGREGAR LA OPCION DE CAMBIAR PIN CON EL OPEN
-            print("2 - Salir del menu")
-            print("Contacto: moreiradanix@gmail.com")
-            ayuda = int(input("Seleccione opcion: "))
+            elif opcion_menu == 1:
+                ConsultarSaldo()
+            elif opcion_menu == 2:
+                CargarSaldo()
+            elif opcion_menu == 3:
+                RetirarSaldo()
 
-        elif opcion == 5:
-            print("Operacion cancelada")
-            break
-    except ValueError:
-        print("Ingrese una opcion correcta")
+        except ValueError:
+            print("Debe ingresar una opcion correcta")
 
-
-        
-        
+Menu()
